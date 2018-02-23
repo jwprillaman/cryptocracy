6bba7d9c77200de7742e905cff663a93bf72db42
James <jwprillaman@gmail.com>
2018-02-22 22:21:02 -0700

    create election coin

--- cryptocracy.go
+++ cryptocracy.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	//"io"
+	"fmt"
 	"net/http"
 )
 
--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var Voter = artifacts.require("./Voter.sol");
+var CryptocracyElectionCoin = artifacts.require("./CryptocracyElectionCoin.sol");
 
 module.exports = function(deployer) {
 	deployer.deploy(Voter);
+	deployer.deploy(CryptocracyElectionCoin);
 };
